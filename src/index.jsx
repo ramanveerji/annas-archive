230c904b4da2bab8e0ca12c3150b20aae0875466
Dheison Gomes <dheisomgomes0@gmail.com>
2023-06-02 21:44:07 -0300

    Navigation bar now follow system theme

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,34 @@
 import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { navigationTheme } from './init'
+import { navigationTheme, colors } from './init'
+import * as NavigationBar from 'expo-navigation-bar'
 import HomeScreen from './screens/Home'
 import SearchScreen from './screens/Search'
 
 const Stack = createNativeStackNavigator()
 
-const Main = () => (
-  <NavigationContainer theme={navigationTheme}>
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Home"
-        options={{ title: "Anna's Archive" }}
-        component={HomeScreen}
-      />
-      <Stack.Screen
-        name="Search"
-        options={{ title: 'Procurar...' }}
-        component={SearchScreen}
-      />
-    </Stack.Navigator>
-    <StatusBar style={navigationTheme.dark ? 'light' : 'dark'} />
-  </NavigationContainer>
-)
+const Main = () => {
+  const reverseTheme = navigationTheme.dark ? 'light' : 'dark'
+  NavigationBar.setBackgroundColorAsync(colors.background)
+  NavigationBar.setButtonStyleAsync(reverseTheme)
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          options={{ title: "Anna's Archive" }}
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="Search"
+          options={{ title: 'Procurar...' }}
+          component={SearchScreen}
+        />
+      </Stack.Navigator>
+      <StatusBar style={reverseTheme} />
+    </NavigationContainer>
+  )
+}
 
 export default Main
